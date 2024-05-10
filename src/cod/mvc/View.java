c953ba1d3eb7ae5940be133cdd5a25dec8f72709
Chema <jcaobaqueiro@danielcastelao.org>
2024-05-10 09:51:40 +0200

    Nueva clase View para mostrar la velocidad

---
+++ src/cod/mvc/View.java
@@ -0,0 +1,17 @@
+package cod.mvc;
+
+public class View {
+
+    /**
+     * Metodo en el que se recibe la velocidad y la matricula
+     *
+     * @param matricula identificador del vehículo
+     * @param velocidad velocidad del vehículo
+     * @return
+     */
+    public static boolean muestraVelocidad(String matricula, int velocidad) {
+
+
+        return true;
+    }
+}
