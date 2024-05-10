b811ca1be5ea1a5bf9d555e3af263235aac3c5d3
Chema <jcaobaqueiro@danielcastelao.org>
2024-05-10 08:33:27 +0200

    Test

---
+++ src/Test/ModelTest.java
@@ -0,0 +1,29 @@
+import cod.mvc.Coche;
+import cod.mvc.Model;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ModelTest {
+    @Test
+    public void crearCocheReturnTrue(){
+        Assertions.assertNotNull(Model.crearCoche("matricula", "modelo", 20));
+    }
+
+    @Test
+    public void getCocheAssertSameReturnTrue(){
+        Coche coche = Model.crearCoche("modelo", "matricula", 20);
+        Model.parking.add(coche);
+        Assertions.assertEquals(Model.getCoche("matricula"), coche);
+    }
+
+    @Test
+    public void cambiarVelocidadComprobarCambioReturnTrue(){
+        Coche coche = Model.crearCoche("modelo", "matricula", 20);
+        Integer velocidad = 40;
+        Model.parking.add(coche);
+        Model.cambiarVelocidad("matricula", velocidad);
+        Assertions.assertEquals(velocidad, coche.getVelocidad());
+
+    }
+
+}
