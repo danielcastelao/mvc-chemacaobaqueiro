5b1dfc3403609e51baa84242a3139e4fa93fac8e
Chema <jcaobaqueiro@danielcastelao.org>
2024-05-24 10:09:02 +0200

    Cambio de las variables

--- src/cod/mvc/model/Coche.java
+++ src/cod/mvc/model/Coche.java
@@ -2,37 +2,13 @@ package cod.mvc.model;
 
 public class Coche {
 
-    private String matricula;
-    private String modelo;
-    private Integer velocidad;
+    public String matricula;
+    public String modelo;
+    public Integer velocidad;
 
-    public Coche(String modelo, String matricula, Integer velocidad) {
+    public Coche(String modelo, String matricula) {
         this.matricula = matricula;
         this.modelo = modelo;
-        this.velocidad = velocidad;
-    }
-
-    public String getModelo() {
-        return modelo;
-    }
-
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
-
-    public String getMatricula() {
-        return matricula;
-    }
-
-    public void setMatricula(String matricula) {
-        this.matricula = matricula;
-    }
-
-    public Integer getVelocidad() {
-        return velocidad;
-    }
-
-    public void setVelocidad(Integer velocidad) {
         this.velocidad = velocidad;
     }
 }
