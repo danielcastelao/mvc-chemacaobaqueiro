d1013bf1e671af1a4b9f50aebc8f7778947460a1
Chema <jcaobaqueiro@danielcastelao.org>
2024-05-24 10:10:28 +0200

    Cambio del ObserverLimite para modificar la velocidad

--- src/cod/mvc/controller/ObserverLimite.java
+++ src/cod/mvc/controller/ObserverLimite.java
@@ -1,18 +1,21 @@
 package cod.mvc.controller;
 
 import cod.mvc.model.Coche;
+import cod.mvc.model.Model;
 
-public class ObserverLimite implements Observer{
+public class ObserverLimite implements Observer {
 
-    final static int vlimite = 200;
+    final static int vlimite = 300;
 
 
     @Override
-    public void update(Coche arg) {
+    public void update(Coche arg, Model model) {
 
-        if (arg.getVelocidad() > vlimite) {
-            System.out.println(arg.getVelocidad());
+        if (arg.velocidad > vlimite) {
             System.out.println("(ObserverLimite) Velocidad por encima del límite");
+            model.cambiarVelocidad(arg.matricula, arg.velocidad-10);
+            System.out.println("(ObserverLimite) Reducción de velocidad");
         }
     }
+
 }
