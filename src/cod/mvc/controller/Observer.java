3544586b8e117cc50b32a234908b3b94c7e906f6
Chema <jcaobaqueiro@danielcastelao.org>
2024-05-24 10:09:37 +0200

    Cambio del observer para que reciba un model

--- src/cod/mvc/controller/Observer.java
+++ src/cod/mvc/controller/Observer.java
@@ -1,8 +1,9 @@
 package cod.mvc.controller;
 import cod.mvc.model.Coche;
+import cod.mvc.model.Model;
 
 public interface Observer {
 
-    void update(Coche arg);
+    void update(Coche arg,Model model);
 
 }
