13299b546760bf8cdf8054542154bfa278ffe85c
Chema <jcaobaqueiro@danielcastelao.org>
2024-05-24 10:11:39 +0200

    Cambios en el model (aún hay cosas que arreglar)

--- src/cod/mvc/model/Model.java
+++ src/cod/mvc/model/Model.java
@@ -5,65 +5,9 @@ import cod.mvc.controller.Observer;
 
 public class Model implements Observable{
 
-
     public static ArrayList<Coche> parking = new ArrayList<>();
 
-    /**
-     * Metodo en el que se crea un coche
-     * @param newModelo modelo del coche
-     * @param newMatricula identificador del coche
-     * @param newVelocidad velocidad del coche
-     * @return array con los coches
-     */
-
-    public static Coche crearCoche(String newModelo, String newMatricula, Integer newVelocidad) {
-
-        Coche newCoche = new Coche(newModelo, newMatricula, newVelocidad);
-        return newCoche;
-    }
-
-    /**
-     * Metodo para sacar el coche creado anteriormente
-     * @param matricula identificador del coche
-     * @return devuelve el coche asociado a la mátricula introducida
-     */
-
-    public static Coche getCoche(String matricula) {
-        for (Coche coche : parking) {
-            if (coche.getMatricula().equals(matricula)) {
-                return coche;
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Metodo en el cual se puede cambiar la velocidad de un coche
-     *
-     * @param matricula identificador del coche
-     * @param velocidad velocidad a la que la quieres cambiar
-     * @return devuelve la velocidad
-     */
-
-    public static boolean cambiarVelocidad(String matricula, Integer velocidad) {
-        Coche coche = getCoche(matricula);
-        coche.setVelocidad(velocidad);
-        return true;
-    }
-
-    /**
-     * Metodo para sacar la velocidad de un coche
-     * @param matricula identificador del coche
-     * @return devuelve la velocidad del coche
-     */
-
-    public static Integer getVelocidad(String matricula) {
-        Coche coche = getCoche(matricula);
-        return coche.getVelocidad();
-    }
-
-    private static final ArrayList<Observer> observers = new ArrayList<Observer>();
+    private static final ArrayList<Observer> observers = new ArrayList<>();
 
     @Override
     public void addObserver(Observer observer) {
@@ -82,12 +26,60 @@ public class Model implements Observable{
     }
 
 
-    public static ArrayList<Coche> getParking() {
-        return parking;
+
+    /**
+     * Metodo en el que se crea un coche
+     * @param newModelo modelo del coche
+     * @param newMatricula identificador del coche
+     * @return array con los coches
+     */
+
+    public static Coche crearCoche(String newModelo, String newMatricula) {
+
+        Coche newCoche = new Coche(newModelo, newMatricula);
+        parking.add(newCoche);
+        return newCoche;
     }
 
-    public static void setParking(ArrayList<Coche> parking) {
-        Model.parking = parking;
+    /**
+     * Metodo para sacar el coche creado anteriormente
+     * @param matricula identificador del coche
+     * @return devuelve el coche asociado a la mátricula introducida
+     */
+
+    public static Coche getCoche(String matricula) {
+        Coche newCoche = null;
+        for (Coche coche : parking) {
+            if (coche.matricula.equals(matricula)) {
+                newCoche = coche;
+            }
+        }
+        return newCoche;
     }
+
+    /**
+     * Metodo en el cual se puede cambiar la velocidad de un coche
+     *
+     * @param matricula identificador del coche
+     * @param velocidad velocidad a la que la quieres cambiar
+     */
+
+    public void cambiarVelocidad(String matricula, Integer velocidad) {
+        getCoche(matricula).velocidad = velocidad;
+        notifyObservers(getCoche(matricula));
+    }
+
+    /**
+     * Metodo para sacar la velocidad de un coche
+     * @param matricula identificador del coche
+     * @return devuelve la velocidad del coche
+     */
+
+    public static Integer getVelocidad(String matricula) {
+        Coche coche = getCoche(matricula);
+        return coche.velocidad;
+    }
+
+
 
 }
