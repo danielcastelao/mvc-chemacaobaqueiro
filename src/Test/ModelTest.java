ecdab2e6b794f44c76404afbc877e40ad8253df3
Chema <jcaobaqueiro@danielcastelao.org>
2024-05-24 09:02:29 +0200

    Nuevo test para cambiar velocidad y diga si se pasa del limite

--- src/Test/ModelTest.java
+++ src/Test/ModelTest.java
@@ -1,9 +1,18 @@
 import cod.mvc.model.Coche;
 import cod.mvc.model.Model;
+import cod.mvc.controller.Controller;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ModelTest {
+
+    private Model model;
+    private Controller controller;
     @Test
     public void crearCocheReturnTrue(){
         Assertions.assertNotNull(Model.crearCoche("matricula", "modelo", 20));
@@ -13,7 +22,7 @@ public class ModelTest {
     public void getCocheAssertSameReturnTrue(){
         Coche coche = Model.crearCoche("modelo", "matricula", 20);
         Model.parking.add(coche);
-        Assertions.assertEquals(Model.getCoche("matricula"), coche);
+        assertEquals(Model.getCoche("matricula"), coche);
     }
 
     @Test
@@ -22,8 +31,21 @@ public class ModelTest {
         Integer velocidad = 40;
         Model.parking.add(coche);
         Model.cambiarVelocidad("matricula", velocidad);
-        Assertions.assertEquals(velocidad, coche.getVelocidad());
+        assertEquals(velocidad, coche.getVelocidad());
 
     }
 
+    @Test
+    public void cambiarVelocidadComprobarCambioReturnFalse(){
+        ByteArrayOutputStream salida = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(salida));
+
+        controller.crearCoche("ModeloTest","MatriculaTest",120);
+        controller.cambiarVelocidad("MatriculaTest",350);
+        Coche coche = Model.getCoche("MatriculaTest");
+        assertEquals(350, coche.getVelocidad());
+
+        String salidaTest = "Se ha cambiado la velocidad 350km/h\n"+"MatriculaTest: 350km/h\n"+"Infraccion\n";
+        assertEquals(salidaTest, salida.toString());
+    }
 }
