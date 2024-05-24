a9163185c99da4e06766d98a04a00c66669510b7
Chema <jcaobaqueiro@danielcastelao.org>
2024-05-24 10:12:12 +0200

    Cambios necesarios al realizar cambios en la clase coche

--- src/cod/mvc/controller/Controller.java
+++ src/cod/mvc/controller/Controller.java
@@ -1,24 +1,13 @@
 package cod.mvc.controller;
 
-import cod.mvc.model.Coche;
+
 import cod.mvc.model.Model;
-import cod.mvc.view.View;
+
 
 public class Controller {
 
     private final Model miModel;
-    /**
-     * Nueva clase controller para crear un nuevo coche y mostrarlo por pantalla
-     */
-    public static void controladorCoches(){
 
-        Model.crearCoche("zl1", "0765-VGF", 250);
-        Coche miCoche = Model.getCoche("0765-VGF");
-
-
-        System.out.println(View.muestraVelocidad(miCoche.getMatricula(), miCoche.getVelocidad()));
-
-    }
     public Controller(Model miModel) {
         this.miModel = miModel;
 
@@ -29,8 +18,8 @@ public class Controller {
         miModel.addObserver(obsLim);
     }
 
-    public void crearCoche(String modelo, String matricula, int velocidad) {
-        miModel.crearCoche(matricula,modelo,velocidad);
+    public void crearCoche(String modelo, String matricula) {
+        miModel.crearCoche(matricula,modelo);
     }
 
     public void cambiarVelocidad(String matricula, Integer velocidad) {
